1f6ed498b649e2d5aed0416279e28e83c7988c18
TrickierBroom8 <67897229+TrickierBroom8@users.noreply.github.com>
2020-09-08 22:18:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Verander geen van de volgende dingen zonder dat je weet wat je doet.. voor hulp kan je altijd Oscar.#7370 contacteren.
 const Discord = require('discord.js')
 
 const client = new Discord.Client();
@@ -10,7 +11,7 @@ if (config.token === 'HIER-JOUW-TOKEN') return console.log('De bot heeft geen ge
 
 client.once('ready', () => {
     console.log('Deze bot is nu online')
-
+// Verander hier onder tussen de haakjes wat je de status van de bot wilt zijn.
     client.user.setActivity('Deze bot', { type: "WATCHING" })
 });
 
@@ -18,6 +19,9 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
+
+// Je kan hieronder commands toevoegen door na de } else if (command === 'NAAM') { Codering } te gebruiken.
+// Voor hulp contacteer: Oscar.#7370
 
     if (command === 'ping') {
         message.channel.send('pong!');
