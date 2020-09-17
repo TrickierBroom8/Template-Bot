16920b55f43dfc5e6b72a63c035146d3255b5417
TrickierBroom8 <67897229+TrickierBroom8@users.noreply.github.com>
2020-09-17 11:35:44 +0200

    Update index.js
    Command Handler update.. logs which commandfiles are loaded.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-
+    console.log(`${file} geladen!`);
     client.commands.set(command.name, command);
 }
 
