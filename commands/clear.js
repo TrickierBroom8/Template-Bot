13e8d93b8979db29b14b95f87bb833ba5cfb218d
TrickierBroom8 <67897229+TrickierBroom8@users.noreply.github.com>
2020-11-12 11:00:51 +0100

    Update clear.js
    Comments toegevoegd

--- commands/clear.js
+++ commands/clear.js
@@ -3,27 +3,27 @@ module.exports = {
     description: "Verwijder een bepaald aantal berichten.",
     async execute(message, args) {
 
-        message.delete()
+        message.delete() // Verwijderd het eerste bericht
 
-        if (!args[0]) return message.channel.send('Geef een nummer op');
+        if (!args[0]) return message.channel.send('Geef een nummer op'); // Kijkt of er wel argumenten zijn meegegeven ?clear args[0]
 
-        let number = args.slice(0).join(" ");
+        let number = args.join(" "); // Haalt de argumenten op
 
-        if (!number) return message.channel.send('Geef een nummer op');
+        if (!number) return message.channel.send('Geef een nummer op'); // Checkt of er een nummer is
 
-        if (isNaN(number)) return message.channel.send('Geef een nummer op');
+        if (isNaN(number)) return message.channel.send('Geef een nummer op'); // Checkt of het argument een nummer is
 
-        if (number > 100) return message.channel.send('Je kan niet meer dan 100 berichten verwijderen');
+        if (number > 100) return message.channel.send('Je kan niet meer dan 100 berichten verwijderen'); // Checkt of het nummer groter is als 100
 
-        if (number < 2) return message.channel.send('Je kan niet minder als 2 berichten verwijderen');
+        if (number < 2) return message.channel.send('Je kan niet minder als 2 berichten verwijderen'); // Checkt of het nummer kleiner is als 2
 
-        await message.channel.messages.fetch({ limit: number }).then(async messages => {
-            message.channel.send('Aan het verwijderen..').then(msg =>{
-                message.channel.bulkDelete(messages, true).then(messages => {
-                    msg.edit(`${messages.size} berichten verwijderd.`);
+        await message.channel.messages.fetch({ limit: number }).then(async messages => { // Haalt de berichten uit dat kanaal op.
+            message.channel.send('Aan het verwijderen..').then(msg => { // Stuurt het aan het verwijderen bericht
+                message.channel.bulkDelete(messages, true).then(messages => { // Verwijdert de berichten 
+                    msg.edit(`\`${messages.size}\` berichten verwijderd.`); // Edit het aan het verwijderen bericht naar x aantal berichten verwijderd
                 });
-                msg.delete({ timeout: 7500 })
-            }); 
+                msg.delete({ timeout: 7500 }) // Verwijdert het bericht na 7,5 seconden
+            });
         });
     }
 }
